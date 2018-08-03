f721e08f477b7d7f6b1fdc60664d062f17988057
wei <2061469002@qq.com>
2018-08-03 16:18:42 +0800

    实现查询功能

--- js/map.js
+++ js/map.js
@@ -1,14 +1,12 @@
 // 创建map对象
-
+var familiesFilter = []
+var generaFilter = []
 function Map(id) {
   this.map = new BMap.Map(id, {
     mapType: BMAP_NORMAL_MAP
   })
-  this.infoWindowConfig = {
-    width: 450,
-    height: 400
-  }
   this.plantData = null
+  this.plantLayer = null
 }
 
 Map.prototype.config = function() {
@@ -54,23 +52,25 @@ Map.prototype.init = function() {
   this.config()
   this.addControl()
   this.addPlant()
-  this.search('木耳', '木耳')
+  this.fgInit()
   //this._getPlantData()
 }
 
 Map.prototype.addPlant = function() {
-  var plantLayer = new BMap.CustomLayer({
+  this.plantLayer = new BMap.CustomLayer({
     geotableId: 192795,
     q: '', // 检索关键字
     tages: '', // 空格分隔的多字符串
-    fliter: '' // 过滤条件
+    filter: '' // 过滤条件
   })
-
-  this.map.addTileLayer(plantLayer)
+  this.map.addTileLayer(this.plantLayer)
 
   // 增加点击事件监听 ，点击返回信息窗口
 
-  plantLayer.addEventListener('onhotspotclick', this._showMessage.bind(this))
+  this.plantLayer.addEventListener(
+    'onhotspotclick',
+    this._showMessage.bind(this)
+  )
 }
 
 Map.prototype._createWindowHtml = function(info) {
@@ -137,11 +137,6 @@ Map.prototype._showMessage = function(e) {
   })
   searchInfoWindow.open(point)
   // this.map.openInfoWindow(infoWindow, point)
-
-  var img = document.querySelector('#img-container img')
-  img.onload = function() {
-    searchInfoWindow.redraw()
-  }
 }
 
 Map.prototype._getPlantData = function() {
@@ -162,6 +157,34 @@ Map.prototype._getPlantDataById = function(id) {
 }
 
 Map.prototype.search = function(f, g) {
+  var filter = []
+  if (f) {
+    filter.push('familiesId:[' + f + ']')
+  }
+  if (g) {
+    filter.push('generaId:[' + g + ']')
+  }
+  console.log(filter.join('|'))
+  if (this.plantLayer) {
+    this.map.removeTileLayer(this.plantLayer)
+    console.log('remove')
+  }
+  this.plantLayer = new BMap.CustomLayer({
+    geotableId: 192795,
+    q: '', // 检索关键字
+    tages: '', // 空格分隔的多字符串
+    filter: filter.join('|') // 过滤条件
+  })
+
+  this.map.addTileLayer(this.plantLayer)
+  this.plantLayer.addEventListener(
+    'onhotspotclick',
+    this._showMessage.bind(this)
+  )
+  // TODO: 为什么使用fetch就无法做到
+}
+
+Map.prototype.fgInit = function() {
   var url = 'http://api.map.baidu.com/geosearch/v3/local?callback=?'
   $.ajax({
     type: 'GET',
@@ -170,18 +193,77 @@ Map.prototype.search = function(f, g) {
     data: {
       q: '', //检索关键字
       region: '中南民族大学', //北京的城市id
+      filter: '',
       scope: '2', //显示详细信息
       geotable_id: 192795,
       ak: 'xSD7rlMlk5GKIjWzKjfOAquu0hmBVQMX' //用户ak
     },
     dataType: 'json',
     success: function(res) {
-      console.log(res)
+      var content = res.contents
+      content.forEach(value => {
+        _createElement(value.families, value.familiesId, '.families')
+        _createElement(value.genera, value.generaId, '.genera')
+      })
+
+      var allFamilies = document.querySelectorAll('.families .name')
+      var allGenera = document.querySelectorAll('.genera .name')
+
+      allFamilies.forEach(function(item) {
+        item.addEventListener('click', clickListenerF, false)
+      })
+
+      allGenera.forEach(function(item) {
+        item.addEventListener('click', clickListenerG, false)
+      })
+      function clickListenerF() {
+        this.classList.toggle('active')
+        var id = this.getAttribute('id')
+        var flag = familiesFilter.findIndex((value, index) => {
+          return id === value
+        })
+        if (flag === -1) {
+          familiesFilter.push(id)
+        } else {
+          familiesFilter.splice(flag, 1)
+        }
+
+        // var filter = []
+        // if (familiesFilter) {
+        //   filter.push('familiesId:[' + familiesFilter + ']')
+        // } else if (generaFilter) {
+        //   filter.push('generaId:[' + generaFilter + ']')
+        // } else {
+        //   console.log('请输入查询条件')
+        // }
+      }
+
+      function clickListenerG() {
+        this.classList.toggle('active')
+        var id = this.getAttribute('id')
+        var flag = familiesFilter.findIndex(value => {
+          return id === value
+        })
+        if (flag === -1) {
+          generaFilter.push(id)
+        } else {
+          generaFilter.splice(flag, 1)
+        }
+      }
+      function _createElement(name, id, fg) {
+        var span = document.createElement('span')
+        span.classList.add('name')
+        var text = document.createTextNode(name)
+        var num = document.createAttribute('id')
+        num.value = id
+        span.setAttributeNode(num)
+        span.appendChild(text)
+        var container = document.querySelectorAll(fg)[0]
+        container.appendChild(span)
+      }
     },
     error: function() {
-      console.log('error')
+      // console.log('error')
     }
   })
-
-  // TODO: 为什么使用fetch就无法做到
 }
