0a3f4afe58fc554a896bf0d63463d78ce2b50270
bad coder <2061469002@qq.com>
2018-12-14 13:10:49 +0800

    修改图片上传按钮的显示形式

--- map/src/components/addImage.vue
+++ map/src/components/addImage.vue
@@ -22,16 +22,19 @@
                  multiple
                  :limit="8"
                  accept="image/*"
-                 :disabled="form.name === ''"
-                 :action="imgAction"
+                 :disabled="uploadFlag"
+                 :action="
+                 imgAction"
                  :before-upload="beforeUpload">
         <div slot="tip"
              class="el-upload__tip">只能上传图片文件，且不超过2MB，最多上传文件数为8，建议图片长宽比在2:1左右，且宽度大于700</div>
         <el-button slot="trigger"
+                   :disabled="uploadFlag"
                    type="primary">选取图片</el-button>
         <el-button style="margin-left: 50px;"
                    type="success"
                    :loading="uploading"
+                   :disabled="uploadFlag"
                    @click="submitUpload">上传服务器</el-button>
       </el-upload>
       <div class="img-preview-container">
@@ -71,159 +74,169 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import cropper from '@/components/cropper'
+import { mapGetters } from "vuex";
+import cropper from "@/components/cropper";
 export default {
-  name: 'addImage',
+  name: "addImage",
   data() {
     return {
       cropIndex: null,
       cropType: null,
-      imgAction: 'http://59.68.29.67:8000/api/uploadFile',
+      imgAction: "http://59.68.29.67:8000/api/uploadFile",
       form: {
-        name: ''
+        name: ""
       },
       param: new FormData(),
       filesArr: [],
       uploading: false,
       showCropper: false,
-      imgUrl: null
-    }
+      imgUrl: null,
+      uploadFlag: true
+    };
   },
   components: {
     cropper
   },
   computed: {
-    ...mapGetters(['nameOptions'])
+    ...mapGetters(["nameOptions"])
   },
   methods: {
     beforeUpload(file) {
       // 判断文件大小是否满足要求
-      const isLt2M = file.size / 1024 / 1024 < 2
-      const isIMG = /^image\//.test(file.type)
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      const isIMG = /^image\//.test(file.type);
       if (!isLt2M) {
-        this.$message.error('上传文件大小不能超过2MB！')
+        this.$message.error("上传文件大小不能超过2MB！");
       } else if (!isIMG) {
-        this.$message.error('上传文件必须是图片格式！')
+        this.$message.error("上传文件必须是图片格式！");
       } else {
         let imgFile = {
           url: URL.createObjectURL(file),
           name: file.name,
           data: file
-        }
+        };
         // 判断图片的尺寸大小
         this.getSize(imgFile.url).then(res => {
           if (res.width < 700) {
             this.$notify({
-              type: 'error',
+              type: "error",
               duration: 8000,
-              title: '建议重新上传',
+              title: "建议重新上传",
               message: `${imgFile.name}像素过低，可能导致显示模糊！`
-            })
+            });
           } else {
-            let ratio = Math.round(res.width / res.height)
+            let ratio = Math.round(res.width / res.height);
             if (ratio !== 2) {
               this.$notify({
-                type: 'warning',
+                type: "warning",
                 duration: 8000,
-                title: '建议裁剪',
+                title: "建议裁剪",
                 message: `${imgFile.name}长宽比不满足2:1，可能导致显示变形`
-              })
+              });
             }
           }
-        })
-        this.filesArr.push(imgFile)
+        });
+        this.filesArr.push(imgFile);
       }
-      return false
+      return false;
     },
     httpRequest() {},
     submitUpload() {
-      this.uploading = true
-      let formData = new FormData()
-      formData.append('name', '木耳3')
+      this.uploading = true;
+      let formData = new FormData();
+      formData.append("name", "木耳3");
       // this.param.append('name', this.form.name)
       this.filesArr.forEach(item => {
-        formData.append('file', item.data, item.name)
-      })
+        formData.append("file", item.data, item.name);
+      });
       let config = {
         headers: {
-          'Content-Type': 'multipart/form-data'
+          "Content-Type": "multipart/form-data"
         }
-      }
+      };
       this.$axios
-        .post('/uploadFile', formData, config)
+        .post("/uploadFile", formData, config)
         .then(res => {
-          this.$message.success('图片上传成功')
+          this.$message.success("图片上传成功");
         })
         .catch(err => {
-          console.error(err)
-          this.$message.error('图片上传失败')
+          console.error(err);
+          this.$message.error("图片上传失败");
         })
         .finally(() => {
           // 重置所有数据
-          this.filesArr = []
-          this.uploading = false
-          this.param.delete('file')
-          this.param.delete('name')
-        })
+          this.filesArr = [];
+          this.uploading = false;
+          this.param.delete("file");
+          this.param.delete("name");
+        });
     },
     getSize(url) {
       return new Promise((resolve, reject) => {
-        let img = new Image()
-        img.src = url
-        document.body.appendChild(img)
+        let img = new Image();
+        img.src = url;
+        document.body.appendChild(img);
         img.onload = () => {
-          let w = img.offsetWidth
-          let h = img.offsetHeight
-          document.body.removeChild(img)
+          let w = img.offsetWidth;
+          let h = img.offsetHeight;
+          document.body.removeChild(img);
           resolve({
             width: w,
             height: h
-          })
-        }
-      })
+          });
+        };
+      });
     },
     tailor(file, index) {
-      this.imgUrl = file.url
-      this.showCropper = true
-      this.cropIndex = index
-      this.cropType = file.type
+      this.imgUrl = file.url;
+      this.showCropper = true;
+      this.cropIndex = index;
+      this.cropType = file.type;
     },
     closeCropper(...value) {
-      this.showCropper = value[0]
+      this.showCropper = value[0];
     },
     cropperUrlChange(...value) {
-      this.imgUrl = value[0]
+      this.imgUrl = value[0];
     },
     cropSuccess(data) {
-      let url = window.URL.createObjectURL(data)
-      let name = this.filesArr[this.cropIndex].name
+      let url = window.URL.createObjectURL(data);
+      let name = this.filesArr[this.cropIndex].name;
       let file = new File([data], name, {
         type: this.cropType,
         lastModified: Date.now()
-      })
-      this.filesArr[this.cropIndex].url = url
-      this.filesArr[this.cropIndex].data = file
+      });
+      this.filesArr[this.cropIndex].url = url;
+      this.filesArr[this.cropIndex].data = file;
     },
     reupload(index) {
       this.getFile().then(res => {
         if (res.length) {
-          this.filesArr[index].url = URL.createObjectURL(res[0])
-          this.filesArr[index].name = res[0].name
-          this.filesArr[index].data = res[0]
+          this.filesArr[index].url = URL.createObjectURL(res[0]);
+          this.filesArr[index].name = res[0].name;
+          this.filesArr[index].data = res[0];
         }
-      })
+      });
     },
     getFile() {
       return new Promise((resolve, reject) => {
-        this.$refs.uploadFile[0].click()
-        this.$refs.uploadFile[0].addEventListener('change', function() {
-          resolve(this.files)
-        })
-      })
+        this.$refs.uploadFile[0].click();
+        this.$refs.uploadFile[0].addEventListener("change", function() {
+          resolve(this.files);
+        });
+      });
+    }
+  },
+  watch: {
+    // 由于form是一个对象，我们现在如果想要监听form，就需要使用深度监听的模式
+    form: {
+      handler(newV, oldV) {
+        this.uploadFlag = false;
+      },
+      deep: true
     }
   }
-}
+};
 </script>
 
 <style>
