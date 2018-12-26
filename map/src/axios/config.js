2ee0e69ec9bb759bf1b88ee17b41bd6470e382ab
bad coder <2061469002@qq.com>
2018-12-26 10:36:55 +0800

    fix img problem

--- map/src/axios/config.js
+++ map/src/axios/config.js
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: 'http://59.68.29.67:8000/api', // api的基地址 // 响应的最大限制时间
+  baseURL: 'http://59.68.29.81:8000/api', // api的基地址 // 响应的最大限制时间
   // 设置http的头部信息
   headers: { 'Content-Type': 'application/json;charset=UTF-8' }
 }
